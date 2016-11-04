79e07f02bd3ae323e58afa1108581e1119b11b9f
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2016-11-05 00:27:06 +0200

    Update module

--- src/main/java/com/cdvdev/commons/fragment/CustomDialogFragment.java
+++ src/main/java/com/cdvdev/commons/fragment/CustomDialogFragment.java
@@ -27,10 +27,22 @@ public abstract class CustomDialogFragment extends DialogFragment {
     @LayoutRes
     public abstract int getLayoutResId();
 
-    @StyleRes
-    public abstract int getDialogAnimationStyle();
+    public abstract View populateView(View layoutView);
 
     public abstract boolean isLayoutDifferentForOrientation();
+
+    @StyleRes
+    public int getDialogAnimationStyle() {
+        return 0;
+    }
+
+    @Nullable
+    public Bundle saveStateForPopulateView() {
+        return null;
+    }
+
+    public void restoreStateForPopulateView(@Nullable Bundle savedState) {
+    }
 
     @Nullable
     @Override
@@ -41,8 +53,10 @@ public abstract class CustomDialogFragment extends DialogFragment {
         if (container != null) {
             container.removeAllViewsInLayout();
         }
-        View view = inflater.inflate(getLayoutResId(), container, false);
-        return populateView(view, savedInstanceState);
+        View view = populateView(inflater.inflate(getLayoutResId(), container, false));
+        //apply saved state
+        restoreStateForPopulateView(mSavedInstanceState);
+        return view;
     }
 
     @Override
@@ -64,6 +78,9 @@ public abstract class CustomDialogFragment extends DialogFragment {
         super.onConfigurationChanged(newConfig);
         if (!isLayoutDifferentForOrientation()) return;
         if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT || newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //save view state
+            Bundle savedState = saveStateForPopulateView();
+
             LayoutInflater inflater = LayoutInflater.from(getActivity());
             //switch portrait or landscape layout on configuration changed
             //remove fragment view from container
@@ -71,12 +88,12 @@ public abstract class CustomDialogFragment extends DialogFragment {
             if (container != null) {
                 container.removeAllViewsInLayout();
             }
+            //populate view
             View view = inflater.inflate(getLayoutResId(), container, true);
-            populateView(view, mSavedInstanceState);
+            populateView(view);
+            //apply saved state
+            restoreStateForPopulateView(savedState != null ? savedState : mSavedInstanceState);
         }
     }
-
-
-    public abstract View populateView(View layoutView, @Nullable Bundle savedInstanceState);
 
 }
---
+++ src/main/java/com/cdvdev/commons/fragment/CustomFullScreenDialogFragment.java
@@ -0,0 +1,22 @@
+package com.cdvdev.commons.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Custom dialog fragment with no title bar and with translucent
+ * <p>
+ * Created by Dmitriy V. Chernysh on 04.11.16.
+ * dmitriy.chernysh@gmail.com
+ * <p>
+ * www.mobile-dev.pro
+ */
+
+public abstract class CustomFullScreenDialogFragment extends CustomDialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NO_FRAME, android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+    }
+}
