17759d1b5e75a9a4a83f9d4663b922a6a0ec6ed3
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2016-12-17 20:01:37 +0200

    Added base Apllication class

---
+++ src/main/java/com/cdvdev/commons/BaseApplication.java
@@ -0,0 +1,29 @@
+package com.cdvdev.commons;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Base class for Application main class
+ * <p>
+ * Created by Dmitriy V. Chernysh on 17.12.16.
+ * dmitriy.chernysh@gmail.com
+ * <p>
+ * www.mobile-dev.pro
+ */
+
+public class BaseApplication extends Application {
+    private static BaseApplication sApp;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (sApp == null) {
+            sApp = this;
+        }
+    }
+
+    public static Context getAppContext() {
+        return sApp.getApplicationContext();
+    }
+}
