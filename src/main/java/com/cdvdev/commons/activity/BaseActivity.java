523d093a8aa4faa5abab3392e2d4410930f7ba14
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2016-12-17 20:31:48 +0200

    Updated BaseActivity

--- src/main/java/com/cdvdev/commons/activity/BaseActivity.java
+++ src/main/java/com/cdvdev/commons/activity/BaseActivity.java
@@ -2,7 +2,9 @@ package com.cdvdev.commons.activity;
 
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -11,11 +13,26 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Dmitriy V. Chernysh on 23.05.16.
  * dmitriy.chernysh@gmail.com
  */
-public class BaseActivity extends AppCompatActivity implements IBaseActivity {
+public abstract class BaseActivity extends AppCompatActivity implements IBaseActivity {
+
+    protected FragmentManager mFragmentManager;
+
+    protected abstract void initToolbar();
+
+    @LayoutRes
+    protected abstract int getLayoutResId();
+
+    protected abstract void populateView();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        initToolbar();
+
+        mFragmentManager = getSupportFragmentManager();
+
+        populateView();
     }
 
     @Override
