d6653b566d3262c8c476572981594cbf663b191f
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2017-02-09 17:09:47 +0200

    Updated Base fragments - set method for change  icon of the home/up indicator

--- src/main/java/com/cdvdev/commons/fragment/BaseFragment.java
+++ src/main/java/com/cdvdev/commons/fragment/BaseFragment.java
@@ -3,6 +3,7 @@ package com.cdvdev.commons.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.MenuRes;
 import android.support.annotation.NonNull;
@@ -55,12 +56,15 @@ public abstract class BaseFragment extends Fragment {
         String titleString = getAppBarTitleString();
         @StringRes int titleResId = getAppBarTitle();
         @ColorRes int colorResId = getAppBarColor();
+        @DrawableRes int homeIcon = getHomeAsUpIndicatorIcon();
 
         if (!(activity instanceof IBaseActivity)) {
             if (colorResId > 0)
                 throw new UnsupportedOperationException("Your activity should extends from 'com.cdvdev.commons.activity.BaseActivity' for set AppBar color");
             if (titleResId > 0 || !titleString.equals(""))
                 throw new UnsupportedOperationException("Your activity should extends from 'com.cdvdev.commons.activity.BaseActivity' for set AppBar title");
+            if (homeIcon > 0)
+                throw new UnsupportedOperationException("Your activity should extends from 'com.cdvdev.commons.activity.BaseActivity' for set home indicator icon");
         } else {
 
             if (titleResId > 0) {
@@ -69,6 +73,8 @@ public abstract class BaseFragment extends Fragment {
                 ((IBaseActivity) activity).setAppBarTitle(titleString);
             }
             if (colorResId > 0) ((IBaseActivity) activity).setAppBarColor(colorResId);
+
+            if (homeIcon > 0) ((IBaseActivity) activity).setHomeAsUpIndicatorIcon(homeIcon);
         }
 
     }
@@ -102,4 +108,10 @@ public abstract class BaseFragment extends Fragment {
     protected int getOptionsMenuResId() {
         return 0;
     }
+
+    @DrawableRes
+    protected int getHomeAsUpIndicatorIcon() {
+        return 0;
+    }
+
 }
--- src/main/java/com/cdvdev/commons/fragment/BaseFragmentDifferentOrientation.java
+++ src/main/java/com/cdvdev/commons/fragment/BaseFragmentDifferentOrientation.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.MenuRes;
 import android.support.annotation.NonNull;
@@ -93,12 +94,15 @@ public abstract class BaseFragmentDifferentOrientation extends Fragment {
         String titleString = getAppBarTitleString();
         @StringRes int titleResId = getAppBarTitle();
         @ColorRes int colorResId = getAppBarColor();
+        @DrawableRes int homeIcon = getHomeAsUpIndicatorIcon();
 
         if (!(activity instanceof IBaseActivity)) {
             if (colorResId > 0)
                 throw new UnsupportedOperationException("Your activity should extends from 'com.cdvdev.commons.activity.BaseActivity' for set AppBar color");
             if (titleResId > 0 || !titleString.equals(""))
                 throw new UnsupportedOperationException("Your activity should extends from 'com.cdvdev.commons.activity.BaseActivity' for set AppBar title");
+            if (homeIcon > 0)
+                throw new UnsupportedOperationException("Your activity should extends from 'com.cdvdev.commons.activity.BaseActivity' for set home indicator icon");
         } else {
 
             if (titleResId > 0) {
@@ -107,6 +111,8 @@ public abstract class BaseFragmentDifferentOrientation extends Fragment {
                 ((IBaseActivity) activity).setAppBarTitle(titleString);
             }
             if (colorResId > 0) ((IBaseActivity) activity).setAppBarColor(colorResId);
+
+            if (homeIcon > 0) ((IBaseActivity) activity).setHomeAsUpIndicatorIcon(homeIcon);
         }
 
     }
@@ -136,6 +142,11 @@ public abstract class BaseFragmentDifferentOrientation extends Fragment {
 
     @MenuRes
     protected int getOptionsMenuResId() {
+        return 0;
+    }
+
+    @DrawableRes
+    protected int getHomeAsUpIndicatorIcon() {
         return 0;
     }
 }
